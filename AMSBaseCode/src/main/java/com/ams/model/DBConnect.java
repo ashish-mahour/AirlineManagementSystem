4ef7f8fb82eca5c1dae887eb1503781f9f78dba9
Ashish Mahour <amashishmahour@gmail.com>
2018-07-09 13:37:55 +0530

    DB class only

--- AMSBaseCode/src/main/java/com/ams/model/DBConnect.java
+++ AMSBaseCode/src/main/java/com/ams/model/DBConnect.java
@@ -2,10 +2,12 @@ package com.ams.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnect {
-	public static Connection getConnection() {
+	private static Connection getLocalConnection() {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -19,6 +21,28 @@ public class DBConnect {
 			e.printStackTrace();
 		}
 		return conn;
+	}
+
+	public static Statement getStatement() {
+		Statement statement = null;
+		try {
+			statement = getLocalConnection().createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return statement;
+	}
+
+	public static PreparedStatement getPreparedStatement(String sqlQuery) {
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = getLocalConnection().prepareStatement(sqlQuery);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return preparedStatement;
 	}
 
 }
